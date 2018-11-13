17d272862a187fdd7d9d9a9e7ac4e424f2c83df2
Maria Sá <dud.franco11@gmail.com>
2018-11-12 22:00:57 -0200

    Initial commit

---
+++ script.sql
@@ -0,0 +1,17 @@
+
+/**
+ * Author:  51118022018.1
+ * Created: 12/11/2018
+ */
+
+create database sga;
+
+  use sga;
+
+  create table tb_aluno(
+    id int primary key auto_increment,
+    nome varchar(150) not null,
+    email varchar(150) not null,
+    matricula int not null,
+    curso varchar(50) not null
+  );
